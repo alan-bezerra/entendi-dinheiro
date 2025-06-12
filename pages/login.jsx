839110aae5763392fae238eb7623dbdd9c1619b0
Raiane Oliveira <raiane.oliveira404@gmail.com>
2025-06-11 21:54:00 -0300

    feat: página de login implementada por Samuel

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,48 @@
 function LoginPage() {
   return (
-    <div>
-      <h1>Perfil Page</h1>
-      <p>Bem-vindo ao Entendi Dinheiro!</p>
-      <p>Ir para o <Link href="/">Home</Link></p>
+    <div className="auth-container app-container">
+      <div className="auth-logo">
+        <img src="../assets/logo.svg" alt="Entendi Dinheiro Logo" />
+      </div>
+
+      <h2 className="auth-title">Acesse sua conta</h2>
+
+      <div className="social-login">
+        <button className="social-btn google">
+          <i className="ph ph-google-logo"></i>
+          Google
+        </button>
+        <button className="social-btn facebook">
+          <i className="ph ph-facebook-logo"></i>
+          Facebook
+        </button>
+      </div>
+
+      <form id="login-form">
+        <label className="form-label" htmlFor="email">E-mail</label>
+        <input className="form-input" type="email" id="email" name="email" placeholder="johndoe@example.com" />
+
+        <label className="form-label" htmlFor="senha">Senha</label>
+        <input className="form-input" type="password" id="senha" name="senha" placeholder="Sua senha" />
+
+        <button type="submit" className="login-btn">Entrar</button>
+      </form>
+
+      <div className="divider"></div>
+
+      <div className="signup">
+        <div className="signup-text">
+          <i className="ph ph-currency-circle-dollar"></i>
+          <p>
+            <span>Não tem uma conta?</span>
+            <Link href="/cadastro">Crie sua conta grátis</Link>
+          </p>
+        </div>
+
+        <i className="ph ph-caret-right signup-arrow-icon"></i>
+      </div>
     </div>
+
   )
 }
 
